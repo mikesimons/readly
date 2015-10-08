170e95f4d85ef49686aea3cba8276e39ae3a611a
Mike Simons <mike@mikesimons.co.uk>
2015-10-08 17:26:22 +0100

    gofmt / vet fixes

--- readly.go
+++ readly.go
@@ -1,68 +1,72 @@
 package readly
 
-import(
-  "io"
-  "io/ioutil"
-  "net/http"
-  "os"
-  "regexp"
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
 )
 
-var httpClient *http.Client;
+var httpClient *http.Client
 
 // Credit to http://play.golang.org/p/ps7PXFRI2B
 type nopReader struct{}
+
 func (nopReader) Read(p []byte) (n int, err error) { return len(p), nil }
 
-func isHttp(file string) bool {
-  http := regexp.MustCompile("^http(s)?:")
-  return http.MatchString(file)
+func isHTTP(file string) bool {
+	http := regexp.MustCompile("^http(s)?:")
+	return http.MatchString(file)
 }
 
+// SetClient will force the local http client instance to use
 func SetClient(c *http.Client) {
-  httpClient = c
+	httpClient = c
 }
 
 func client() *http.Client {
-  var nilClient *http.Client
-  if httpClient == nilClient {
-    httpClient = &http.Client{}
-  }
-  return httpClient
+	var nilClient *http.Client
+	if httpClient == nilClient {
+		httpClient = &http.Client{}
+	}
+	return httpClient
 }
 
-// TODO: Error wrapping
-// TODO: timeout?
+// Reader will return an io.ReadCloser for either a file or http(s) URL
 func Reader(file string) (io.ReadCloser, error) {
-  nilReaderCloser := ioutil.NopCloser(&nopReader{})
-  if isHttp(file) {
-    resp, err := client().Get(file)
-    if err != nil {
-      return nilReaderCloser, err
-    }
-    return resp.Body, nil
-  } else {
-    file, err := os.Open(file)
-    if err != nil {
-      return nilReaderCloser, err
-    }
-    return file, nil
-  }
+	// TODO: Error wrapping
+	// TODO: timeout?
+	nilReaderCloser := ioutil.NopCloser(&nopReader{})
+	if isHTTP(file) {
+		resp, err := client().Get(file)
+		if err != nil {
+			return nilReaderCloser, err
+		}
+		return resp.Body, nil
+	}
+
+	fileHandle, err := os.Open(file)
+	if err != nil {
+		return nilReaderCloser, err
+	}
+	return fileHandle, nil
 }
 
+// Read will take a file or http(s) URL, read all contents and return them
 func Read(file string) (string, error) {
-  r, err := Reader(file)
-  defer r.Close()
+	r, err := Reader(file)
+	defer r.Close()
 
-  if err != nil {
-    return "", err
-  }
+	if err != nil {
+		return "", err
+	}
 
-  ret, err := ioutil.ReadAll(r)
+	ret, err := ioutil.ReadAll(r)
 
-  if err != nil {
-    return "", err
-  }
+	if err != nil {
+		return "", err
+	}
 
-  return string(ret), nil
+	return string(ret), nil
 }
